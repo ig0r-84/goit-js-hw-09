4a51d88d9b56bbb54ba6b29ec3296190d8bc270b
ig0r-84 <coshilnick@gmail.com>
2026-07-17 01:39:57 +0300

    add 2 task

---
+++ src/js/2-form.js
@@ -0,0 +1,50 @@
+const form = document.querySelector('.feedback-form');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+
+  formData.email = parsedData.email || '';
+  formData.message = parsedData.message || '';
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener('input', handleInput);
+
+function handleInput(event) {
+  const field = event.target.name;
+
+  formData[field] = event.target.value.trim();
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  form.reset();
+
+  formData.email = '';
+  formData.message = '';
+}
